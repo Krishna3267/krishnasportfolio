27b18e1d8d056831d75554ea5e33aa90bf416991
Krishna3267 <krishnashah2607@gmail.com>
2021-11-19 00:47:45 +0530

    changes made

--- js/script.js
+++ js/script.js
@@ -16,3 +16,14 @@ var typed = new Typed(".typing3", {
     typeSpeed: 100,
     loop: true,
   });
+
+var navbar = document.getElementById("navbar-nav");
+var link = navbar.getElementsByClassName("nav-item");
+var an = link.getElementsByClassName("nav-link");
+for (var i = 0; i < link.length; i++) {
+  link[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
