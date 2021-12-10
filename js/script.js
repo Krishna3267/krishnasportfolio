8ce25acaabee2ceb23bab527cae46cbd93393e67
Krishna3267 <krishnashah2607@gmail.com>
2021-12-10 14:28:56 +0530

    improved

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
-var typed = new Typed(".typing", {
-    strings: ["Tech Enthusiast", "Music Lover", "Cricket Lover", "Potter Head", "Cinephile"],
-    typeSpeed: 100,
-    // backSpeed: 100,
-    loop: true,
-  });
+// var typed = new Typed(".typing", {
+//     strings: ["Tech Enthusiast", "Music Lover", "Cricket Lover", "Potter Head", "Cinephile"],
+//     typeSpeed: 100,
+//     // backSpeed: 100,
+//     loop: true,
+//   });
 
 var typed = new Typed(".typing2", {
-    strings: ["Fresher at IIIT Allahabad"],
+    strings: ["IT Sophomore at IIIT Allahabad"],
 
     typeSpeed: 100,
   });
 
-var typed = new Typed(".typing3", {
-    strings: ["Hola, Hello, Namaste"],
-    typeSpeed: 100,
-    loop: true,
-  });
+// var typed = new Typed(".typing3", {
+//     strings: ["Hola, Hello, Namaste"],
+//     typeSpeed: 100,
+//     loop: true,
+//   });
 
 var navbar = document.getElementById("navbar-nav");
 var link = navbar.getElementsByClassName("nav-item");
