8a66f7cbb63d59b8497a1f91aa6dccc96cb2f868
Krishna3267 <krishnashah2607@gmail.com>
2021-12-12 19:47:36 +0530

    little changes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-var typed = new Typed(".typing", {
-    strings: ["Tech Enthusiast", "Music Lover", "Cricket Lover", "Potter Head", "Cinephile"],
-    typeSpeed: 100,
-    // backSpeed: 100,
-    loop: true,
-  });
+// var typed = new Typed(".typing", {
+//     strings: ["Tech Enthusiast", "Music Lover", "Cricket Lover", "Potter Head", "Cinephile"],
+//     typeSpeed: 100,
+//     // backSpeed: 100,
+//     loop: true,
+//   });
 
 var typed = new Typed(".typing2", {
     strings: ["IT Sophomore at IIIT Allahabad"],
@@ -11,11 +11,11 @@ var typed = new Typed(".typing2", {
     typeSpeed: 100,
   });
 
-var typed = new Typed(".typing3", {
-    strings: ["Hola, Hello, Namaste"],
-    typeSpeed: 100,
-    loop: true,
-  });
+// var typed = new Typed(".typing3", {
+//     strings: ["Hola, Hello, Namaste"],
+//     typeSpeed: 100,
+//     loop: true,
+//   });
 
 var navbar = document.getElementById("navbar-nav");
 var link = navbar.getElementsByClassName("nav-item");
